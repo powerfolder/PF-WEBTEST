56c435940cc76007f9a43c2c4218ae2a0b89564c
EL-HAITI YOUNES <elhaiti.younes@gmail.com>
2022-03-27 13:26:49 +0000

    Add links test suit

---
+++ Test Suites/Links.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Links</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>47e5f5d8-0151-4a6d-9ea0-f99e7a84e8ee</testSuiteGuid>
+   <testCaseLink>
+      <guid>70214f91-f178-405b-88d7-234c2297a716</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFolderLink with expirary date</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7b7cba63-479c-42e2-a423-085610af536a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFileLink - with max downloads</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ccfb0555-963c-4e6d-9c71-e1adbb60f73f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFolderLink - with password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5245317b-b031-484e-8299-914bf77d041f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFileLink</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d355182f-b879-4b5d-b9e6-4cf9c88d7e21</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFolderLink</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1a553969-0277-4b8c-9ff8-a277d567ec0b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFileLink - with expire date</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>656c8d00-c3a4-4cd0-a0eb-4f8b51509634</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFileLink - with password</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6a61c604-e5b6-4f07-84a0-497fe18ef23c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createFolderLink - with max downloads</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>3cd783ea-e835-46f9-9453-5d71bb3315ab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Links/createDirLink</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
