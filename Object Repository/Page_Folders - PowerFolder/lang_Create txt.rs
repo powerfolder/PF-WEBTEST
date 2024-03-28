34c807cd5b8f4461d1a45602dd9c50d9eca733da
YOUNES EL-HAITI <elhaiti.younes@gmail.com>
2024-03-28 23:02:58 +0000

    Fix links tests'

---
+++ Object Repository/Page_Folders - PowerFolder/lang_Create txt.rs
@@ -0,0 +1,59 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>lang_Create txt</name>
+   <tag></tag>
+   <elementGuidId>424654c8-a84b-41cd-954e-d547add0ebb0</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='pica-files-create-dropdown']/ul/li[4]/a/lang</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>li.files-visible-only-other.files-ui-create-doc > a > lang[name=&quot;string_create&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>lang</value>
+      <webElementGuid>933952bd-f852-42c6-88d6-8df447df7e99</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>name</name>
+      <type>Main</type>
+      <value>string_create</value>
+      <webElementGuid>dcdf2a57-6c85-4f1f-91c6-2bb4b793fd6b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Create Document</value>
+      <webElementGuid>b1ed5a3e-26bc-4878-9160-b9f054498cd8</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;pica-files-create-dropdown&quot;)/ul[@class=&quot;pica-arrow-left dropdown-menu&quot;]/li[@class=&quot;files-visible-only-other files-ui-create-doc&quot;]/a[1]/lang[1]</value>
+      <webElementGuid>0dd0510c-c25d-41f1-8bd6-4cb4b54f9658</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='pica-files-create-dropdown']/ul/li[4]/a/lang</value>
+      <webElementGuid>7a4d7cb3-78b7-40b6-85e8-2978450dac12</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
