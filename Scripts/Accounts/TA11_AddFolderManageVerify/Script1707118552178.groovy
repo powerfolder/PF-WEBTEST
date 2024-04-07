de4eab1e3130db65baf552689327caa252dcc290
Tarikelhaiti <tarik.elhaiti1996@gmail.com>
2024-04-07 22:50:23 +0000

    fix

--- Scripts/Accounts/TA11_AddFolderManageVerify/Script1707118552178.groovy
+++ Scripts/Accounts/TA11_AddFolderManageVerify/Script1707118552178.groovy
@@ -1,13 +1,14 @@
-import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
-import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
-import org.openqa.selenium.By as By
-import org.openqa.selenium.JavascriptExecutor as JavascriptExecutor
-import org.openqa.selenium.Keys as Keys
-import org.openqa.selenium.WebDriver as WebDriver
-import org.openqa.selenium.WebElement as WebElement
-import com.kms.katalon.core.model.FailureHandling as FailureHandling
-import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
-import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCaseFactory
+import com.kms.katalon.core.testobject.ObjectRepository
+import com.kms.katalon.core.webui.driver.DriverFactory
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords
+import org.openqa.selenium.By
+import org.openqa.selenium.JavascriptExecutor
+import org.openqa.selenium.Keys
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+
 import java.util.Random
 
 String firstName = generateRandomString(8)
@@ -16,56 +17,55 @@ String emailId = generateRandomEmail()
 Emailid = emailId.toLowerCase()
 String phone = generateRandomPhoneNumber()
 
-WebUI.callTestCase(findTestCase('Folders/PreTest_GoToShareable'), [:], FailureHandling.OPTIONAL)
-WebUI.click(findTestObject('LeftNavigationIcons/account'))
-WebUI.click(findTestObject('Accounts/CreateButton'))
-WebUI.click(findTestObject('Accounts/ClickCreateAccount'))
-WebUI.setText(findTestObject('Accounts/InputUserOrEmail'), emailId)
-WebUI.setText(findTestObject('Accounts/InputPassword'), 'Alexa@131190')
-WebUI.setText(findTestObject('Accounts/InputFirstName'), firstName)
-WebUI.setText(findTestObject('Accounts/InputLastName'), lastName)
-WebUI.setText(findTestObject('Accounts/InputPhoneNo'), phone)
-WebUI.setText(findTestObject('Accounts/InputQuota'), '5')
-WebUI.click(findTestObject('Accounts/SaveButton'))
-WebUI.delay(1)
-WebUI.setText(findTestObject('Accounts/inputAccountSearch'), firstName)
-WebUI.sendKeys(findTestObject('Accounts/inputAccountSearch'), Keys.chord(Keys.ENTER))
+WebUiBuiltInKeywords.callTestCase(TestCaseFactory.findTestCase('Folders/PreTest_GoToShareable'), [:], FailureHandling.OPTIONAL)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('LeftNavigationIcons/account'))
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/CreateButton'))
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/ClickCreateAccount'))
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputUserOrEmail'), emailId)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputPassword'), 'Alexa@131190')
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputFirstName'), firstName)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputLastName'), lastName)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputPhoneNo'), phone)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/InputQuota'), '5')
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/SaveButton'))
+WebUiBuiltInKeywords.delay(1)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/inputAccountSearch'), firstName)
+WebUiBuiltInKeywords.sendKeys(ObjectRepository.findTestObject('Accounts/inputAccountSearch'), Keys.chord(Keys.ENTER))
 
 WebDriver driver = DriverFactory.getWebDriver()
 
 try {
     WebElement ClickOnAccount = driver.findElement(By.partialLinkText(firstName))
-    WebUI.delay(1)
+    WebUiBuiltInKeywords.delay(1)
     ClickOnAccount.click()
 } catch (Exception e) {
     WebElement ClickOnAccount = driver.findElement(By.xpath("//a[@class='pica-name' and @data-original-title ='$emailId']"))
-    JavascriptExecutor executor = ((driver) as JavascriptExecutor)
-    executor.executeScript('arguments[0].click()', ClickOnAccount)
+WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(ClickOnAccount))
 } 
 
-WebUI.verifyEqual(WebUI.getText(findTestObject('Accounts/VerifyEditPage')), 'Edit Account', FailureHandling.CONTINUE_ON_FAILURE)
-WebUI.click(findTestObject('Accounts/AddFolder'))
-WebUI.setText(findTestObject('Accounts/AddFolderByNameInput'), firstName)
-WebUI.delay(2)
-WebUI.click(findTestObject('Accounts/SelectCreateNew'))
-WebUI.delay(1)
-WebUI.click(findTestObject('Accounts/SelectFolder'))
-WebUI.click(findTestObject('Accounts/SaveButton'))
-WebUI.delay(2)
-String actualText = WebUI.getText(findTestObject('Accounts/VerifyAlertMsg'))
+WebUiBuiltInKeywords.verifyEqual(WebUiBuiltInKeywords.getText(ObjectRepository.findTestObject('Accounts/VerifyEditPage')), 'Edit Account', FailureHandling.CONTINUE_ON_FAILURE)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/AddFolder'))
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/AddFolderByNameInput'), firstName)
+WebUiBuiltInKeywords.delay(2)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/SelectCreateNew'))
+WebUiBuiltInKeywords.delay(1)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/SelectFolder'))
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/SaveButton'))
+WebUiBuiltInKeywords.delay(2)
+String actualText = WebUiBuiltInKeywords.getText(ObjectRepository.findTestObject('Accounts/VerifyAlertMsg'))
 String expectedText = 'Account Updated'
-WebUI.delay(2)
-WebUI.setText(findTestObject('Accounts/inputAccountSearch'), firstName)
-WebUI.waitForElementClickable(findTestObject('Accounts/checkAccount'), 1, FailureHandling.OPTIONAL)
-WebUI.click(findTestObject('Accounts/checkAccount'))
-WebUI.click(findTestObject('Accounts/DeleteButton'))
-WebUI.delay(2)
-String actualDeletedText = WebUI.getText(findTestObject('Accounts/VerifyDeleteMsg'))
+WebUiBuiltInKeywords.delay(2)
+WebUiBuiltInKeywords.setText(ObjectRepository.findTestObject('Accounts/inputAccountSearch'), firstName)
+WebUiBuiltInKeywords.waitForElementClickable(ObjectRepository.findTestObject('Accounts/checkAccount'), 1, FailureHandling.OPTIONAL)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/checkAccount'))
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/DeleteButton'))
+WebUiBuiltInKeywords.delay(2)
+String actualDeletedText = WebUiBuiltInKeywords.getText(ObjectRepository.findTestObject('Accounts/VerifyDeleteMsg'))
 String expectedDeletedText = "Do you really want to delete $emailId?"
-WebUI.verifyEqual(actualDeletedText, expectedDeletedText, FailureHandling.STOP_ON_FAILURE)
-WebUI.click(findTestObject('Accounts/YesButton'))
-WebUI.delay(2)
-WebUI.closeBrowser()
+WebUiBuiltInKeywords.verifyEqual(actualDeletedText, expectedDeletedText, FailureHandling.STOP_ON_FAILURE)
+WebUiBuiltInKeywords.click(ObjectRepository.findTestObject('Accounts/YesButton'))
+WebUiBuiltInKeywords.delay(2)
+WebUiBuiltInKeywords.closeBrowser()
 
 String generateRandomString(int length) {
     String characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
