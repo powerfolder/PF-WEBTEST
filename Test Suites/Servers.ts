f67ad015f549d071c470ba1de4bf947b7c425f7a
Christian Sprajc <sprajc@powerfolder.com>
2024-05-07 22:47:30 +0200

    Renamed

--- Test Suites/Servers.ts
+++ Test Suites/Servers.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>server</name>
+   <name>Servers</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
