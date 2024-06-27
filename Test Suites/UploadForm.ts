b9d791c7705036a0402099b16cfdaf55e9ce712f
Tarikelhaiti <tarik.elhaiti1996@gmail.com>
2024-06-27 19:14:45 +0100

    raname test cases

--- Test Suites/UploadForm.ts
+++ Test Suites/UploadForm.ts
@@ -15,7 +15,7 @@
       <guid>7ed0a801-1904-4f02-93ac-da19e1b72f37</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP01_Create an upload form</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF01_Create an upload form</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -28,35 +28,35 @@
       <guid>719b5122-705b-4907-a152-a96bf8ed30ac</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP02_Delete an upload from</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF02_Delete an upload from</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>90391524-8f62-4b8c-a1d4-1db0a55fd877</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP03_Upload a file by Upload form</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF03_Upload a file by Upload form</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>76c8219e-953c-483b-8428-bc2ab530aea1</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP04_Upload a directory</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF04_Upload a directory</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>3af4d188-3bc4-4212-9ef0-93aa2a16f392</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP05_Change upload form</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF05_Change upload form</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b46272a1-930d-467c-b4d4-09f2d79561cf</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP06_Connect with password</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF06_Connect with password</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
          <testDataLinkId></testDataLinkId>
@@ -69,7 +69,7 @@
       <guid>e47707ef-32a1-43cc-8666-0b8f38c1db25</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/Upload form/TUP07_Connect with two sessions</testCaseId>
+      <testCaseId>Test Cases/Upload form/TUF07_Connect with two sessions</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
