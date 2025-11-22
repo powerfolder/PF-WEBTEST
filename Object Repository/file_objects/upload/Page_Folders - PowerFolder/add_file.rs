5ecdac841ee06a9e3af1b544271122a549355913
Tarikelhaiti <tarik.elhaiti1996@gmail.com>
2025-11-22 15:38:22 +0100

    start image deep link

---
+++ Object Repository/file_objects/upload/Page_Folders - PowerFolder/add_file.rs
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>add_file</name>
+   <tag></tag>
+   <elementGuidId>3ca8708a-ff91-422f-bbc6-ec370b7a6b52</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body/div[2]/div[1]/div[2]/div[5]/div/div/div[3]/div/div[1]/span[1]&#xd;
+</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
