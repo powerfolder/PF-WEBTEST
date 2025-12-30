61475206eecfcc82f4a28ac12c7056038941fce6
PF-bughunter <martin.staude1@web.de>
2025-12-30 19:43:08 +0100

    PFS-4749: standardize e-mail in testcases for Registration

--- Scripts/Registration/TR1_VerifyRegistrationTest/Script1706775744019.groovy
+++ Scripts/Registration/TR1_VerifyRegistrationTest/Script1706775744019.groovy
@@ -111,7 +111,7 @@ String generateRandomString(int length) {
 }
 
 String generateRandomEmail() {
-    return generateRandomString(8) + '@email.com'
+    return generateRandomString(8) + '@qa-automated-webtest.com'
 }
 
 String generateRandomPhoneNumber() {
