af63e505984e1b32fad18d079bc42dd2a5b9a4ee
Tarikelhaiti <tarik.elhaiti1996@gmail.com>
2026-01-08 14:36:00 +0100

    PFS-4740 fix webtest: TD02 - test dashboard of org-admin

---
+++ Object Repository/Accounts/Page_Organizations - PowerFolder/button_Is member.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Is member</name>
+   <tag></tag>
+   <elementGuidId>b2d5612a-9aef-44ec-a252-de207fa61c84</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//body/div[2]/div[1]/div[2]/div[3]/div/div/div[2]/div[6]/div[2]/table/tbody/tr/td[3]/div/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
