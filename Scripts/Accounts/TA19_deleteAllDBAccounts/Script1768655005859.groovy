a78b130b6afdc9bcfcb1e965fd2f4f9e0ab1c98c
PF-bughunter <martin.staude1@web.de>
2026-01-17 14:35:05 +0100

    PFS-4780: add testcases TA18/19 - single/mutli account deletion

---
+++ Scripts/Accounts/TA19_deleteAllDBAccounts/Script1768655005859.groovy
@@ -0,0 +1,115 @@
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import java.awt.Toolkit as Toolkit
+import java.awt.datatransfer.DataFlavor as DataFlavor
+import org.openqa.selenium.By as By
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import java.nio.file.Path as Path
+import java.nio.file.Files as Files
+import java.text.SimpleDateFormat as SimpleDateFormat
+import java.util.Calendar as Calendar
+import java.util.Date as Date
+import org.apache.commons.lang3.RandomStringUtils as RandomStringUtils
+import java.util.Random as Random
+import com.kms.katalon.core.testobject.ConditionType
+
+WebUI.callTestCase(findTestCase('Accounts/Edit_Account/pre_test/Create_Account'), [:], FailureHandling.STOP_ON_FAILURE)
+
+WebUI.click(findTestObject('Accounts/CreateButton'))
+
+WebUI.click(findTestObject('Accounts/ClickCreateAccount'))
+
+WebUI.setText(findTestObject('Accounts/InputUserOrEmail'), GlobalVariable.userEmail)
+
+WebUI.setText(findTestObject('Accounts/InputPassword'), GlobalVariable.Pass)
+
+WebUI.setText(findTestObject('Accounts/InputFirstName'), GlobalVariable.userName)
+
+WebUI.setText(findTestObject('Accounts/InputLastName'), GlobalVariable.userLastName)
+
+WebUI.setText(findTestObject('Accounts/InputPhoneNo'), GlobalVariable.PhoneNumber)
+
+WebUI.setText(findTestObject('My_Account/Overview/Page_Accounts - PowerFolder/account_storage_overwiew'), '5')
+
+WebUI.click(findTestObject('Accounts/SaveButton'))
+
+WebUI.refresh()
+
+WebElement btn = findAccount(GlobalVariable.userEmail)
+
+WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(btn))
+
+WebUI.click(findTestObject('Accounts/select_all_checkbox'))
+
+WebUI.verifyElementPresent(findTestObject('Accounts/DeleteButton'), 2)
+
+WebUI.click(findTestObject('Accounts/DeleteButton'))
+
+WebUI.verifyElementPresent(findTestObject('Accounts/TitelAccountDeleteDialog'), 2)
+
+WebUI.verifyElementPresent(findTestObject('Accounts/TextAccountDeleteDialog'), 2)
+
+WebUI.click(findTestObject('Folders/yesButton_Delete'))
+
+WebUI.verifyElementPresent(findTestObject('Accounts/SuccessAccountDeleteMessage'), 2)
+
+TestObject accountRow = new TestObject()
+accountRow.addProperty(
+    "xpath",
+    ConditionType.EQUALS,
+    "//*[contains(@data-search-keys, '${GlobalVariable.userEmail}')]/td[1]/span"
+)
+
+boolean notPresent = WebUI.waitForElementNotPresent(accountRow, 10)
+WebUI.verifyEqual(notPresent, true)
+
+WebUI.closeBrowser()
+
+WebElement findAccount(String emailId) {
+    WebDriver driver = DriverFactory.getWebDriver()
+
+    return driver.findElement(By.xpath(('//*[contains(@data-search-keys, \'' + emailId) + '\')]/td[1]/span'))
+}
+
+String generateRandomString(int length) {
+	String characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+	StringBuilder randomString = new StringBuilder()
+
+	Random random = new Random()
+
+	for (int i = 0; i < length; i++) {
+		randomString.append(characters.charAt(random.nextInt(characters.length())))
+	}
+	
+	return randomString.toString().toLowerCase()
+}
+
+String generateRandomEmail() {
+	return generateRandomString(8) + '@qa-automated-webtest.com'
+}
+
+String generateRandomPhoneNumber() {
+	Random random = new Random()
+
+	return String.format('(%03d) %03d-%04d', random.nextInt(1000), random.nextInt(1000), random.nextInt(10000))
+}
+
