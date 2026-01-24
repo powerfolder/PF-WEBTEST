9c6322537db5fc9934134fcc2b908ede4ecbb8a0
PF-bughunter <martin.staude1@web.de>
2026-01-24 19:34:59 +0100

    PFS-4776: update Object to match german and eng language

--- Object Repository/Share/Page_Folders - PowerFolder/message_decline_invitation.rs
+++ Object Repository/Share/Page_Folders - PowerFolder/message_decline_invitation.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//div[@id='notification_949007']/div/span[2]</value>
+         <value>//span[(text() = 'Einladung abgelehnt.' or . = 'Einladung abgelehnt.' or .= 'Invitation declined.')]</value>
       </entry>
       <entry>
          <key>CSS</key>
@@ -23,7 +23,7 @@
       <name>tag</name>
       <type>Main</type>
       <value>span</value>
-      <webElementGuid>a6e0e742-3688-4719-a204-00d5f96c445e</webElementGuid>
+      <webElementGuid>a969d2bd-278e-4334-a86b-f9d54cec984f</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>true</isSelected>
@@ -31,7 +31,7 @@
       <name>text</name>
       <type>Main</type>
       <value>Einladung abgelehnt.</value>
-      <webElementGuid>fd057481-5366-48b2-9927-278809eca5ea</webElementGuid>
+      <webElementGuid>b50083a8-9b3f-4477-a648-e270b6db6f67</webElementGuid>
    </webElementProperties>
    <webElementProperties>
       <isSelected>false</isSelected>
@@ -39,7 +39,7 @@
       <name>xpath</name>
       <type>Main</type>
       <value>id(&quot;notification_949007&quot;)/div[@class=&quot;pica-notification-box alert alert-info&quot;]/span[2]</value>
-      <webElementGuid>af4527a7-8cd7-416a-a2c1-fd5c3813fa73</webElementGuid>
+      <webElementGuid>a860641d-136d-490d-9a24-333b4999b51b</webElementGuid>
    </webElementProperties>
    <webElementXpaths>
       <isSelected>true</isSelected>
@@ -47,7 +47,7 @@
       <name>xpath:idRelative</name>
       <type>Main</type>
       <value>//div[@id='notification_949007']/div/span[2]</value>
-      <webElementGuid>7b80f334-ac3c-4aac-b520-b0305f16a04c</webElementGuid>
+      <webElementGuid>046d1a08-bd28-4c39-b5e8-ec007300e361</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -55,7 +55,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='`'])[1]/following::span[2]</value>
-      <webElementGuid>db6b09f6-9081-4d76-a8f4-e8daaea1f26c</webElementGuid>
+      <webElementGuid>c07a0c9e-4627-45d1-8744-ff825f135d42</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -63,7 +63,7 @@
       <name>xpath:neighbor</name>
       <type>Main</type>
       <value>(.//*[normalize-space(text()) and normalize-space(.)='×'])[17]/preceding::span[1]</value>
-      <webElementGuid>388f2f68-a21e-48cc-a1a5-65ec492009c9</webElementGuid>
+      <webElementGuid>12638c26-b745-4773-81bd-82262e36791a</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -71,7 +71,7 @@
       <name>xpath:position</name>
       <type>Main</type>
       <value>//div[3]/div/span[2]</value>
-      <webElementGuid>4085e0be-343c-4594-b3d8-9f530ad0c55a</webElementGuid>
+      <webElementGuid>bf2005b5-328a-41c7-bc69-1a664a76c1d8</webElementGuid>
    </webElementXpaths>
    <webElementXpaths>
       <isSelected>false</isSelected>
@@ -79,6 +79,6 @@
       <name>xpath:customAttributes</name>
       <type>Main</type>
       <value>//span[(text() = 'Einladung abgelehnt.' or . = 'Einladung abgelehnt.')]</value>
-      <webElementGuid>2817b70b-246d-417a-bea5-6cfc02db964b</webElementGuid>
+      <webElementGuid>78f9219d-3f36-4b79-a8e6-49dcb969f4bd</webElementGuid>
    </webElementXpaths>
 </WebElementEntity>
