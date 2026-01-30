50d60337b83c8cc2e1d79133ef40ed43d7bbaeb9
PF-bughunter <martin.staude1@web.de>
2026-01-30 18:03:26 +0100

    PFS-4778: add testcases TG16-18

---
+++ Scripts/Groups/TG18_Try to downgrade admin right for a user by himself/Script1769702877002.groovy
@@ -0,0 +1,200 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.annotation.Keyword as Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberBuiltinKeywords
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as MobileBuiltInKeywords
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGBuiltinKeywords
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WSBuiltInKeywords
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUiBuiltInKeywords
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as WindowsBuiltinKeywords
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import org.apache.commons.lang3.RandomStringUtils as RandomStringUtils
+import com.kms.katalon.core.webui.driver.DriverFactory as DriverFactory
+import org.openqa.selenium.WebDriver as WebDriver
+import org.openqa.selenium.WebElement as WebElement
+import org.openqa.selenium.By as By
+import java.util.Arrays as Arrays
+import org.openqa.selenium.support.ui.ExpectedConditions as ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait as WebDriverWait
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
+
+WebUiBuiltInKeywords.callTestCase(findTestCase('Login/Pretest - Admin Login'), [('variable') : ''], FailureHandling.STOP_ON_FAILURE)
+
+GlobalVariable.userName = (('user_' + RandomStringUtils.randomNumeric(4)) + '@qa-automated-webtest.com')
+
+String user = GlobalVariable.userName
+
+WebUiBuiltInKeywords.click(findTestObject('LeftNavigationIcons/account'))
+
+WebUiBuiltInKeywords.click(findTestObject('Accounts/CreateButton'))
+
+WebUiBuiltInKeywords.click(findTestObject('Accounts/ClickCreateAccount'))
+
+WebUiBuiltInKeywords.setText(findTestObject('Accounts/InputUserOrEmail'), user)
+
+WebUiBuiltInKeywords.setText(findTestObject('Accounts/InputPassword'), GlobalVariable.Pass)
+
+WebUiBuiltInKeywords.setText(findTestObject('My_Account/Overview/Page_Accounts - PowerFolder/account_storage_overwiew'), 
+    '5')
+
+WebUiBuiltInKeywords.click(findTestObject('Accounts/SaveButton'))
+
+WebUI.delay(2)
+
+WebDriver driver = DriverFactory.getWebDriver()
+
+WebElement userElement = driver.findElement(By.xpath(('//td/a[contains(text(),\'' + user) + '\')]'))
+
+boolean isUserCreated = userElement.isDisplayed()
+
+WebUiBuiltInKeywords.verifyEqual(isUserCreated, true)
+
+String groupName = 'Group_' + RandomStringUtils.randomNumeric(4)
+
+GlobalVariable.GroupName = groupName
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Dashboard - PowerFolder/lang_Groups'))
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/Create_group_button'))
+
+WebUiBuiltInKeywords.setText(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/input_Organizations_pica_group_name'), 
+    GlobalVariable.GroupName)
+
+WebUiBuiltInKeywords.setText(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/textarea_Organizations_pica_group_notes'), 
+    'create group')
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/button_Save'))
+
+WebUI.delay(2)
+
+def btn = findGroup(groupName)
+
+WebUiBuiltInKeywords.executeJavaScript('arguments[0].click()', Arrays.asList(btn))
+
+assert groupName != null
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Edit_m'))
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Members'))
+
+WebElement inputElement = driver.findElement(By.xpath('//*[@id=\'pica_group_accounts\']/div[1]/div[1]/input'))
+
+inputElement.sendKeys(user)
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/user click'))
+
+WebUI.delay(2)
+
+// Localisation du bouton via XPath et clic
+def xpath = '/html/body/div[2]/div[1]/div[2]/div[3]/div/div/div[2]/div[4]/div[2]/table/tbody/tr[2]/td[3]/div/button'
+
+//def driver = DriverFactory.getWebDriver()
+def button = driver.findElement(By.xpath(xpath))
+
+WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(button))
+
+WebUI.click(findTestObject('Groups/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Is member and admin'))
+
+WebUiBuiltInKeywords.click(findTestObject('Groups/Page_Groups - PowerFolder/button_Save'))
+
+WebUI.delay(2)
+
+WebUI.click(findTestObject('My_Account/Overview/Page_Accounts - PowerFolder/Icon_account'))
+
+WebUI.click(findTestObject('My_Account/Overview/Page_Accounts - PowerFolder/lang_Log out'))
+
+WebUI.setText(findTestObject('Login/inputEmail'), user)
+
+WebUI.click(findTestObject('Login/loginSubmit'))
+
+WebUI.setText(findTestObject('Login/inputPassword'), GlobalVariable.Pass)
+
+WebUI.click(findTestObject('Login/loginSubmit'))
+
+WebUI.delay(3)
+
+WebUiBuiltInKeywords.click(findTestObject('Object Repository/Groups/Page_Dashboard - PowerFolder/lang_Groups'))
+
+def btn1 = findGroup(groupName)
+
+WebUiBuiltInKeywords.executeJavaScript('arguments[0].click()', Arrays.asList(btn1))
+
+// Navigation vers la section "Edit"
+WebUI.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Edit_m'))
+
+// Navigation vers la section "Members"
+WebUI.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Members'))
+
+// Localisation du bouton dropdown
+def button_1 = driver.findElement(By.xpath(xpath))
+
+WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(button_1))
+
+WebUI.click(findTestObject('Groups/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Is member'))
+
+// Clic sur le bouton "Save"
+WebUI.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/button_Save'))
+
+// Vérification de la présence du message de confirmation de mise à jour du groupe
+WebUI.verifyElementPresent(findTestObject('Groups/Page_Groups - PowerFolder/div_Group updated'), 5)
+
+WebUI.refresh()
+
+WebUI.delay(2)
+
+WebUI.verifyElementText(findTestObject('Groups/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Page_Groups - PowerFolder/Permission'), 
+    'Member') 
+
+
+/*
+
+WebElement btn2 = findGroup(GlobalVariable.GroupName)
+
+WebUiBuiltInKeywords.executeJavaScript('arguments[0].click()', Arrays.asList(btn2))
+
+WebUI.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Edit_m'))
+
+WebUI.click(findTestObject('Object Repository/Groups/Page_Groups - PowerFolder/a_Members'))
+
+def button_2 = driver.findElement(By.xpath(xpath))
+
+WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(button_2))
+
+WebUI.delay(2)
+
+String adminNameOrEmail = 'adminqa' // ou "adminqa@xxx.com" selon ce qui est affiché dans la ligne
+
+TestObject isAdminRoleSelected = new TestObject('isAdminRoleSelected')
+
+isAdminRoleSelected.addProperty('xpath', ConditionType.EQUALS, "//tr[.//td[contains(normalize-space(),'$adminNameOrEmail')]]" +
+	'//div[contains(@class,\'dropdown\') and @data-selected=\'Is member and admin\']')
+
+WebUI.verifyElementPresent(isAdminRoleSelected, 5)
+
+WebUI.closeBrowser()
+
+*/
+
+WebUI.closeBrowser()
+
+@Keyword
+WebElement findGroup(String groupName) {
+    WebDriver driver = DriverFactory.getWebDriver()
+
+    return driver.findElement(By.xpath(('//*[contains(@data-search-keys, \'' + GlobalVariable.GroupName) + '\')]/td[1]/span'))
+}
+
