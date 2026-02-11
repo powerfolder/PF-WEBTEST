150ba44f6f980cc7eac56ca4dfaf1899aad3a8d5
PF-bughunter <martin.staude1@web.de>
2026-02-11 18:53:01 +0100

    PFS-4805: add objects for /linkstable

---
+++ Object Repository/LinksTable/Can read and write linkstab.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Can read and write linkstab</name>
+   <tag></tag>
+   <elementGuidId>d8913a1f-b1ff-431f-9030-0a84184041a3</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id=&quot;pica_link_dialog&quot;]/div/div/div[2]/div[3]/label</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/LinksTable/Delete Link.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Delete Link</name>
+   <tag></tag>
+   <elementGuidId>dcd3585f-9f48-4a34-ba8d-f5d6b828865b</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//body/div[2]/div[1]/div[2]/div[2]/table/thead[1]/tr/th[3]/div/a[3]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
