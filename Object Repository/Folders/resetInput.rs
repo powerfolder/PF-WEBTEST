e533d5d92c985aad266e86190334e3ce95329376
PF-bughunter <martin.staude1@web.de>
2026-05-22 16:33:09 +0200

    PFS-5532: update objects to repair TSF01

--- Object Repository/Folders/resetInput.rs
+++ Object Repository/Folders/resetInput.rs
@@ -7,7 +7,7 @@
    <selectorCollection>
       <entry>
          <key>XPATH</key>
-         <value>//body/div[2]/div[1]/div[2]/div[8]/div/div/div[2]/div/span[2]</value>
+         <value>//div[@id='pica_input_dialog']//span[contains(@class,'pica-input-clear')]</value>
       </entry>
       <entry>
          <key>BASIC</key>
