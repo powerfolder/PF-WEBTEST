4c7f9f887bdbdcbbabe4f710665cd173c2cef8e1
PF-bughunter <martin.staude1@web.de>
2026-05-24 10:21:04 +0200

    PFS-5532: adapt EA06 for dev-build

--- Scripts/Accounts/Edit_Account/EA06_Edit_Quata/Script1738783367647.groovy
+++ Scripts/Accounts/Edit_Account/EA06_Edit_Quata/Script1738783367647.groovy
@@ -32,7 +32,11 @@ println(GlobalVariable.userEmail)
 
 println(GlobalVariable.PhoneNumber)
 
-WebElement btn = findAccount(GlobalVariable.userEmail)
+WebUI.refresh()
+
+WebUI.delay(5)
+
+WebElement btn = findAccount(GlobalVariable.userName)
 
 WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(btn))
 
@@ -44,9 +48,9 @@ WebUI.click(findTestObject('Accounts/SaveButton'))
 
 WebUI.refresh()
 
-WebUI.delay(2)
+WebUI.delay(5)
 
-WebElement btn1 = findAccount(GlobalVariable.userEmail)
+WebElement btn1 = findAccount(GlobalVariable.userName)
 
 WebUI.executeJavaScript('arguments[0].click()', Arrays.asList(btn1))
 
@@ -59,9 +63,13 @@ WebUI.click(findTestObject('Accounts/SaveButton'))
 
 WebUI.closeBrowser()
 
-WebElement findAccount(String emailId) {
+WebElement findAccount(String searchKey) {
     WebDriver driver = DriverFactory.getWebDriver()
+    new org.openqa.selenium.support.ui.WebDriverWait(driver, java.time.Duration.ofSeconds(10)).until(
+        org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated(
+            By.xpath("//table[@id='accounts_table']/tbody/tr[@id]")))
 
-    return driver.findElement(By.xpath(('//*[contains(@data-search-keys, \'' + emailId) + '\')]/td[1]/span'))
+    String xp = "//table[@id='accounts_table']/tbody/tr[contains(@data-search-keys,'" + searchKey + "') or .//a[contains(@title,'" + searchKey + "') or contains(text(),'" + searchKey + "')]]/td[1]/span"
+    return driver.findElement(By.xpath(xp))
 }
 
