0ffecf21f4ea074d2fb15046b4763f077561de3a
PF-bughunter <martin.staude1@web.de>
2026-06-13 21:48:47 +0200

    PFS-5526: add some example testcases for report

--- Test Suites/AccessibilityReport.ts
+++ Test Suites/AccessibilityReport.ts
@@ -12,7 +12,35 @@
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>160c3728-587e-4be4-911f-354a47a8097b</testSuiteGuid>
    <testCaseLink>
-      <guid>4947d77f-abe1-4382-b367-b44b80b890a0</guid>
+      <guid>64a1a7b7-6af6-428c-80e6-e9d9f9b6281c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Accessibility/TAR02_login_page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>de1e262e-eaa7-4568-a658-c98b5c1dbdab</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Accessibility/TAR03_registration</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6e60d3af-4a58-4947-bfb8-665e53098421</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Accessibility/TAR04_folderstable_admin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c9ffbb1f-4f24-410d-ab52-6e7b3e186305</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Accessibility/TAR05_linkstable_admin</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>2d5f5443-7fcd-4011-b733-5859eaf7c332</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/Accessibility/TAR01_Admin_login</testCaseId>
