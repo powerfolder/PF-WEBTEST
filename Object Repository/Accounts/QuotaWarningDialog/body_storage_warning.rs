79a7012073324590dd647b56aa40cb21e83dc941
PF-bughunter <martin.staude1@web.de>
2026-07-15 20:24:24 +0200

    PFS-5629: add testcase TA24

---
+++ Object Repository/Accounts/QuotaWarningDialog/body_storage_warning.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>body_storage_warning</name>
+   <tag></tag>
+   <elementGuidId>0138c498-39cf-4fb9-9656-08675689f024</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//*[@id='pica_confirmation_dialog']//h5[contains(@class,'pica-dialog-description')]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#pica_confirmation_dialog .pica-dialog-description</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
