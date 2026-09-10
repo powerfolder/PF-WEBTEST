bd7bf2c59f5426f660419de3d89ca92e0439cd35
PF-bughunter <martin.staude1@web.de>
2026-09-10 21:33:34 +0200

    PFS-5846: update TG14/15

---
+++ Object Repository/Groups/Page_Groups - PowerFolder/organization_clear.rs
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>organization_clear</name>
+   <tag></tag>
+   <elementGuidId>2db13d98-8b9c-4dae-f41a-2b3c4d5e6f7a</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#pica_group_organization .pica-input-clear</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='pica_group_organization']//span[contains(concat(' ',normalize-space(@class),' '),' pica-input-clear ')]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>span</value>
+      <webElementGuid>3ec24ea9-9c0d-4baf-052b-3c4d5e6f7a8b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>pica-input-clear input-group-text</value>
+      <webElementGuid>4fd35fba-ad1e-4cb0-163c-4d5e6f7a8b9c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='pica_group_organization']/span[2]</value>
+      <webElementGuid>50e46acb-be2f-4dc1-274d-5e6f7a8b9c0d</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
